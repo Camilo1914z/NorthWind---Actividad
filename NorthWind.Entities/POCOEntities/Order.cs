ff2c352796ecb74b556e8bb0e658b4e241e71ea1
Camilo1914z <131234623+Camilo1914z@users.noreply.github.com>
2024-03-29 11:05:26 -0500

    PocoEntitiens,customer,order,product

---
+++ NorthWind.Entities/POCOEntities/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Entities.POCOEntities
+{
+    public class Order
+    {
+    public int Id { get; set; }
+    public string CustomerId { get; set;}
+    public DateTime CustomerDate { get; set;}
+    public string shipCity { get; set;}
+    public string shipCountry { get; set;}
+    public string shipPostalCode { get; set;}
+    }
+}
