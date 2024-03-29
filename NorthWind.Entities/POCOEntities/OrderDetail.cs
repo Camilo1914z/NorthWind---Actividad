4def9fe101c7fdc5f9b99b7f1ee68c5fef58e40e
Camilo1914z <131234623+Camilo1914z@users.noreply.github.com>
2024-03-29 12:32:31 -0500

    OrderDetail,GeneralException

--- NorthWind.Entities/POCOEntities/OrderDetail.cs
+++ NorthWind.Entities/POCOEntities/OrderDetail.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NorthWind.Entities.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,9 @@ namespace NorthWind.Entities.POCOEntities
         public decimal UnitPrice { get; set; }
         public short Quantity { get; set; }
         public Order Order { get; set; }
+        public DiscountTypes DiscountType { get; set; }
+        public double Discount { get; set; }
+        public ShippingTypes ShippingType { get; set; }
 
     }
 }
