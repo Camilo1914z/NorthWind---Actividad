0ae03b8372b1dd7b7a11f9a090b7b5e27829c7ee
Camilo1914z <131234623+Camilo1914z@users.noreply.github.com>
2024-03-29 18:08:40 -0500

    CreateOrderValidator

---
+++ NorthWind.UseCases/CreateOrder/CreateOrderValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using FluentValidation.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.UseCases.CreateOrder
+{
+    public class CreateOrderValidator : AbstractValidator<CreateOrderInputPort>
+    {
+        public CreateOrderValidator()
+        {
+
+
+
+            RuleFor(c => c.CustomerId).NotEmpty()
+                .WithMessage("Debe proporciornar la identificacion del cliente");
+            RuleFor(c => c.ShipAddress).NotEmpty()
+                .WithMessage("Debe proporcionar un correo de envio");
+            RuleFor(c => c.ShipCity).NotEmpty().MinimumLength(3)
+                .WithMessage("Debe proporcionar por lo menos 3 letras de la ciudades");
+            RuleFor(c => c.ShipCountry).NotEmpty().MinimumLength(3)
+                .WithMessage("Debe proporcionar por lo menos 3 caracteres del pais");
+            RuleFor(c => c.OrderDetails)
+                .Must(d => d != null && d.Any())
+                .WithMessage("Debe especificar los productos de la orden");
+        }
+
+
+    }
+}
