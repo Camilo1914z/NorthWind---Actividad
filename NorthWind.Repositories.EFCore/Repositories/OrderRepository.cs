1be38a4ae469fdabecce2b0c5f03c956b28a298a
Camilo1914z <131234623+Camilo1914z@users.noreply.github.com>
2024-03-29 16:02:36 -0500

    OrderRepository, OrderDEtailRepository

---
+++ NorthWind.Repositories.EFCore/Repositories/OrderRepository.cs
@@ -0,0 +1,31 @@
+﻿using NorthWind.Entities.Interfaces;
+using NorthWind.Entities.POCOEntities;
+using NorthWind.Entities.Specifications;
+using NorthWind.Repositories.EFCore.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Repositories.EFCore.Repositories
+{
+    
+  
+    public class OrderRepository : IOrderRepository
+    {
+        readonly NorthWindContext Context;
+
+        public OrderRepository(NorthWindContext context) {  Context = context; }
+        public void create(Order order)
+        {
+            Context.Orders.Add(order);
+        }
+
+        public IEnumerable<Order> GetOrdersBySpecification(Specification<Order> specification)
+        {
+           var ExpressionDeLegate = specification.Expression.Compile();
+            return Context.Orders.Where(ExpressionDeLegate);
+        }
+    }
+}
