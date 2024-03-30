3a72759340564c83aa3663c01f817c1d25d649f5
Camilo1914z <131234623+Camilo1914z@users.noreply.github.com>
2024-03-30 10:33:41 -0500

    NorhtWind.WebExceptionHandler

---
+++ NorhtWind.WebExceptionsPresenter/ExceptionHanlderBase.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorhtWind.WebExceptionsPresenter
+{
+    public class ExceptionHanlderBase
+    {
+        readonly Dictionary<int, string> RFC7231Types = new Dictionary<int, string> {
+            {
+
+
+            StatusCodes.Status500InternalServerError,
+            "http://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1"
+
+            },
+            {
+
+
+            StatusCodes.Status400BadRequest,
+            "http://datatracker.ietf.org/doc/html/rfc7231#section-6.5.1"
+
+            }
+
+
+
+
+
+        };
+
+        public Task SetResult(ExceptionContext context, int?
+            status,string title, string detail) 
+        {
+        ProblemDetails Details = new ProblemDetails
+            {
+                Status = status,
+                Title=title,
+                Type = RFC7231Types.ContainsKey(status.Value)?
+                RFC7231Types[status.Value]:"",
+                Detail=detail
+            
+            
+            
+            };
+
+            context.Result = new ObjectResult(Details)
+            {
+                StatusCode = status
+            };
+            context.ExceptionHandled = true;
+            return Task.CompletedTask;
+
+        }
+
+
+
+
+    }
+}
