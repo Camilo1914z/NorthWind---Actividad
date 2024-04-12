0c2ec76c1c30dbe202f9b7467bb72565247d5ab5
Camilo1914z <131234623+Camilo1914z@users.noreply.github.com>
2024-04-12 16:01:03 -0500

    Changes, and structure update

--- NorthWind.UseCases/Common/Validators/CreateOrderValidator.cs
+++ NorthWind.UseCases/Common/Validators/CreateOrderValidator.cs
@@ -1,29 +1,30 @@
 ﻿using FluentValidation;
-using FluentValidation.Validators;
+using NorthWind.UseCasesDTOs.CreateOrder;
+using NorthWind.UseCasesPorts.CreateOrder;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NorthWind.UseCases.CreateOrder
+namespace NorthWind.UseCases.Common.Validators
 {
-    public class CreateOrderValidator : AbstractValidator<CreateOrderInputPort>
+    public class CreateOrderValidator : AbstractValidator<CreateOrderParams>
     {
         public CreateOrderValidator()
         {
 
 
 
-            RuleFor(c => c.RequestData.CustomerId).NotEmpty()
+            RuleFor(c => c.CustomerId).NotEmpty()
                 .WithMessage("Debe proporciornar la identificacion del cliente");
-            RuleFor(c => c.RequestData.ShipAddress).NotEmpty()
+            RuleFor(c => c.ShipAddress).NotEmpty()
                 .WithMessage("Debe proporcionar un correo de envio");
-            RuleFor(c => c.RequestData.ShipCity).NotEmpty().MinimumLength(3)
+            RuleFor(c => c.ShipCity).NotEmpty().MinimumLength(3)
                 .WithMessage("Debe proporcionar por lo menos 3 letras de la ciudades");
-            RuleFor(c => c.RequestData.ShipCountry).NotEmpty().MinimumLength(3)
+            RuleFor(c => c.ShipCountry).NotEmpty().MinimumLength(3)
                 .WithMessage("Debe proporcionar por lo menos 3 caracteres del pais");
-            RuleFor(c => c.RequestData.OrderDetails)
+            RuleFor(c => c.OrderDetails)
                 .Must(d => d != null && d.Any())
                 .WithMessage("Debe especificar los productos de la orden");
         }
