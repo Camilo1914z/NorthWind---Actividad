e713466336c89046981957575a6236ae47982854
Camilo1914z <131234623+Camilo1914z@users.noreply.github.com>
2024-04-12 00:44:31 -0500

    Correciones

--- NorthWind.UseCases/CreateOrder/CreateOrderValidator.cs
+++ NorthWind.UseCases/CreateOrder/CreateOrderValidator.cs
@@ -15,15 +15,15 @@ namespace NorthWind.UseCases.CreateOrder
 
 
 
-            RuleFor(c => c.CustomerId).NotEmpty()
+            RuleFor(c => c.RequestData.CustomerId).NotEmpty()
                 .WithMessage("Debe proporciornar la identificacion del cliente");
-            RuleFor(c => c.ShipAddress).NotEmpty()
+            RuleFor(c => c.RequestData.ShipAddress).NotEmpty()
                 .WithMessage("Debe proporcionar un correo de envio");
-            RuleFor(c => c.ShipCity).NotEmpty().MinimumLength(3)
+            RuleFor(c => c.RequestData.ShipCity).NotEmpty().MinimumLength(3)
                 .WithMessage("Debe proporcionar por lo menos 3 letras de la ciudades");
-            RuleFor(c => c.ShipCountry).NotEmpty().MinimumLength(3)
+            RuleFor(c => c.RequestData.ShipCountry).NotEmpty().MinimumLength(3)
                 .WithMessage("Debe proporcionar por lo menos 3 caracteres del pais");
-            RuleFor(c => c.OrderDetails)
+            RuleFor(c => c.RequestData.OrderDetails)
                 .Must(d => d != null && d.Any())
                 .WithMessage("Debe especificar los productos de la orden");
         }
