c51bbf323faee2df4f6b5406d1314ff72fe9f23d
Camilo1914z <131234623+Camilo1914z@users.noreply.github.com>
2024-04-11 23:23:35 -0500

    revisando configuraciones

---
+++ NorthWind.WebApi/Startup.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace NorthWind.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // Este método se llama en tiempo de ejecución. Utilice este método para agregar servicios al contenedor.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            // Registro de Swagger para generar la documentación de la API
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Mi API", Version = "v1" });
+            });
+        }
+
+        // Este método se llama en tiempo de ejecución. Utilice este método para configurar el pipeline de solicitud HTTP.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                // Agregar Swagger solo en entorno de desarrollo
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mi API V1");
+                });
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // El error 404 se redirigirá a esta página
+                app.UseStatusCodePagesWithRedirects("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
